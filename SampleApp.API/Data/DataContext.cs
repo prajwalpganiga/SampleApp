7af6661677f62cf503de8df8fa8c442f592ddd39
Prajwal <prajwal.pganiga@comitas.ch>
2020-04-11 21:41:17 +0530

    initial commit

---
+++ SampleApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SampleApp.API.Models;
+
+namespace SampleApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
